16ffd314e60137d9038da4d88fa28f9e5d4a0d3f
Jon Chirinos <chirinosjon@gmail.com>
2022-05-19 17:57:51 -0500

    resolving deployment to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 // connect mongoose to googlebooks database
-mongoose.connect(process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/googlebooks", {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
